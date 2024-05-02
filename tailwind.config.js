0b5c2128601a988c3036f00195c144ddb94caf96
Pratibha Mehta <119779210+pratibha131@users.noreply.github.com>
2024-05-02 18:13:31 +0530

    Add files via upload

---
+++ tailwind.config.js
@@ -0,0 +1,10 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  // all files in dist folder will be seen with * sign 
+  content: ["./dist/*.{html,js}"],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
